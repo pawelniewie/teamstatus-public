98a9cb2a3a99acfb6852d11f70b56367776ccf5b
Pawel Niewiadomski <11110000b@gmail.com>
2013-12-20 06:51:10 +0100

    fixed show not working on firefox

--- assets/javascripts/betasignup.js
+++ assets/javascripts/betasignup.js
@@ -1,7 +1,10 @@
 $(function() {
   $("#signup .loading").hide();
+
   $("form").submit(function(e) {
-    $("#notices div").hide();
+    $("#notices .error").hide();
+    $("#notices .success").hide();
+
     e.preventDefault();
 
     mixpanel && mixpanel.track('Subscribe clicked', {'Email' : $("form input#email").val()});
